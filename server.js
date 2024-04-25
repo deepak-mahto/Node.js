dc305039e1fa531e474e0a03d159f3f5d2853203
Deepak Kumar <mahtodeepak857@gmail.com>
2024-04-25 18:12:52 +0530

    http module and create server

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ import http from "http";
 const PORT = 8000;
 
 const server = http.createServer((req, res) => {
-  res.end("Hello World!");
+  //   res.setHeader("Content-Type", "text/html");
+  //   res.statusCode = 404;
+  res.writeHead(500, { "Content-Type": "application/json" });
+  res.end(JSON.stringify({ message: "Server Error" }));
 });
 
 server.listen(PORT, () => {
