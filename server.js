0dce7d9c6572f93eecee702611d3fb4e78a6bfcc
Deepak Kumar <mahtodeepak857@gmail.com>
2024-04-26 10:26:57 +0530

    NPM scripts, NPM Modules and nodemon, git ignore files, environment variables and .env, req object, marking requests from postman, simple routing, loading files

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
+import { error } from "console";
 import http from "http";
-const PORT = 8000;
+const PORT = process.env.PORT;
 
 const server = http.createServer((req, res) => {
-  //   res.setHeader("Content-Type", "text/html");
-  //   res.statusCode = 404;
-  res.writeHead(500, { "Content-Type": "application/json" });
-  res.end(JSON.stringify({ message: "Server Error" }));
+  try {
+    if (req.method === "GET") {
+      if (req.url === "/") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end("<h1>Homepage</h1>");
+      } else if (req.url === "/about") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end("<h1>About</h1>");
+      } else {
+        res.writeHead(400, { "Content-Type": "text/html" });
+        res.end("<h1>Not Found</h1>");
+      }
+    } else {
+      throw new Error("Method not allowed");
+    }
+  } catch (error) {
+    res.writeHead(500, { "Content-Type": "text/plain" });
+    res.end("<h1>Server Error</h1>");
+  }
 });
 
 server.listen(PORT, () => {
